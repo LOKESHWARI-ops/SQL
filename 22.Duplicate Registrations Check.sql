6c8ad294dcc59aef94811a9f5942c90c607d12ae
LOKESHWARI-ops <lokeshwarimanoharan37@gmail.com>
2025-05-23 15:02:12 +0530

    Update 22.Duplicate Registrations Check.sql

---
+++ 22.Duplicate Registrations Check.sql
@@ -0,0 +1,6 @@
+ELECT u.full_name, e.title, COUNT(r.registration_id) AS registration_count
+FROM Registrations r
+JOIN Users u ON r.user_id = u.user_id
+JOIN Events e ON r.event_id = e.event_id
+GROUP BY r.user_id, r.event_id
+HAVING COUNT(r.registration_id) > 1;
